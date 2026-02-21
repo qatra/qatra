6de066b59100863b327d6239521225dfaf461b78
abdallah-azmy <58990918+abdallah-azmy@users.noreply.github.com>
2026-02-21 19:57:06 +0200

    feat: use fainallogo for web splash screen

--- web/flutter_bootstrap.js
+++ web/flutter_bootstrap.js
@@ -1,3 +1,14 @@
 {{flutter_js}}
 {{flutter_build_config}}
-_flutter.loader.load();
+_flutter.loader.load({
+  onEntrypointLoaded: async function (engineInitializer) {
+    const appRunner = await engineInitializer.initializeEngine();
+    await appRunner.runApp();
+
+    // Remove splash screen after Flutter app runs
+    const splash = document.getElementById("splash");
+    if (splash) {
+      splash.remove();
+    }
+  }
+});
